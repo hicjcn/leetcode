453a527b36a1b666321711d652c54b8cca1bad48
chenjian <hi.cj@outlook.com>
2020-03-09 08:23:11 +0800

    最佳买入时机-动态规划

---
+++ src/main/java/com/jc/leetcode/MaxProfit.java
@@ -0,0 +1,19 @@
+package com.jc.leetcode;
+
+public class MaxProfit {
+
+    public static int maxProfit(int[] prices) {
+        if(prices.length <= 1)
+            return 0;
+        int min = prices[0], max = 0;
+        for(int i = 1; i < prices.length; i++) {
+            max = Math.max(max, prices[i] - min);
+            min = Math.min(min, prices[i]);
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(maxProfit(new int[]{7,1,5,3,6,4}));
+    }
+}
